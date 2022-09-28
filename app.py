152f7363f53d0f98a5b6a8e4e43ca3b4ad88b986
Supratik Mitra <mitra.supratik@gmail.com>
2022-09-28 09:45:46 +0200

    Update in the code

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ Date: September 2022
 Description: API to add copyright sympbol to organization names
 '''
 
+from asyncio import SendfileNotAvailableError
 from flask import Flask, request
 
 app=Flask(__name__)
@@ -21,14 +22,21 @@ def replace_txt(srch_str):
         for loc,item in enumerate(str_list):
             if item in org:
                 rplcd_word = str_list[loc]
-                rplcd_word = rplcd_word+"\u00a9"
-                str_list.pop(loc)
-                str_list.insert(loc, rplcd_word)
+                next_loc=loc+1
+                if next_loc<len(str_list) and str_list[next_loc][0].isupper():
+                    rplcd_word=str_list[next_loc]
+                    rplcd_word = rplcd_word+"\u00a9"
+                    str_list.pop(next_loc)
+                    str_list.insert(next_loc, rplcd_word)
+                else:
+                    rplcd_word = rplcd_word+"\u00a9"
+                    str_list.pop(loc)
+                    str_list.insert(loc, rplcd_word)
         final_str = ' '.join(str_list)
-    except:
-        final_str = 'Error'
+    except :
+        final_str =  'Error'
     
     return final_str
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
